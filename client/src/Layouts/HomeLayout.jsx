b05350766bf021087862b87d21380a157c9133a9
Rakesh-0786 <rakeshkum17253@gmail.com>
2024-09-22 00:04:02 +0530

    Added basic layout and integrated to homepage

--- client/src/Layouts/HomeLayout.jsx
+++ client/src/Layouts/HomeLayout.jsx
@@ -9,7 +9,7 @@ function HomeLayout({ children }) {
     function changeWidth() {
         const drawerSide = document.getElementsByClassName("drawer-side");
         if (drawerSide.length > 0) {
-            drawerSide[0].style.width = "0";
+            drawerSide[0].style.width = 'auto';
         }
     }
 
@@ -18,25 +18,29 @@ function HomeLayout({ children }) {
         if (element.length > 0) {
             element[0].checked = false;
         }
-        changeWidth();
+        const drawerSide = document.getElementsByClassName("drawer-side");
+        if (drawerSide.length > 0) {
+            drawerSide[0].style.width = '0';
+        }
     }
 
     return (
-        <div className="flex flex-col min-h-screen">
-            <div className="drawer z-50 w-fit">
+        <div className="min-h-[90vh]">
+            <div className="drawer fixed top-0 left-0 z-50 w-fit">
                 <input className="drawer-toggle" id="my-drawer" type="checkbox" />
                 <div className="drawer-content">
+                    {/* Ensure this label wraps the menu icon and it is absolutely positioned */}
                     <label htmlFor="my-drawer" className="cursor-pointer relative">
                         <FiMenu
                             onClick={changeWidth}
                             size={"32px"}
-                            className="font-bold text-white m-4"
+                            className="font-bold text-white m-4 absolute top-0 left-0"
                         />
                     </label>
                 </div>
                 <div className="drawer-side w-0">
                     <label htmlFor="my-drawer" className="drawer-overlay"></label>
-                    <ul className="menu p-4 w-48 sm:w-80 bg-base-100 text-base-content relative">
+                    <ul className="menu p-4 w-48 sm:w-80 bg-base-200 text-base-content relative">
                         <li className="w-fit absolute right-2 z-50">
                             <button onClick={hideDrawer}>
                                 <AiFillCloseCircle size={24} />
@@ -64,3 +68,91 @@ function HomeLayout({ children }) {
 }
 
 export default HomeLayout;
+
+
+
+
+
+
+
+
+
+// import React from "react";
+// import { FiMenu } from "react-icons/fi";
+// import { AiFillCloseCircle } from "react-icons/ai";
+// import { Link } from "react-router-dom";
+// import Footer from "../components/Footer";
+
+// function HomeLayout({ children }) {
+
+//     function changeWidth() {
+//         const drawerSide = document.getElementsByClassName("drawer-side");
+//         if (drawerSide.length > 0) {
+//             drawerSide[0].style.width = 'auto';
+//         }
+//     }
+
+//     function hideDrawer() {
+//         const element = document.getElementsByClassName("drawer-toggle");
+//         if (element.length > 0) {
+//             element[0].checked = false;
+//         }
+//         const drawerSide = document.getElementsByClassName("drawer-side");
+//         if (drawerSide.length > 0) {
+//             drawerSide[0].style.width = '0';
+//         }
+
+//         // changeWidth();
+//     }
+
+//     return (
+//         <div className="min-h-[90vh]">
+//             <div className="drawer absolute left-0 z-50 w-fit">
+//                 <input className="drawer-toggle" id="my-drawer" type="checkbox" />
+//                 <div className="drawer-content">
+//                     <label htmlFor="my-drawer" className="cursor-pointer relative">
+//                         <FiMenu
+//                             onClick={changeWidth}
+//                             size={"32px"}
+//                             className="font-bold text-white m-4 absolute top-0 left-0"
+//                         />
+//                     </label>
+//                 </div>
+//                 <div className="drawer-side w-0">
+//                     <label htmlFor="my-drawer" className="drawer-overlay">
+//                     </label>
+//                     <ul className="menu p-4 w-48 sm:w-80 bg-base-100 text-base-content relative">
+//                         <li className="w-fit absolute right-2 z-50">
+//                             <button onClick={hideDrawer}>
+//                                 <AiFillCloseCircle size={24} />
+//                             </button>
+//                         </li>
+//                         <li>
+//                             <Link to="/">Home</Link>
+//                         </li>
+//                         <li>
+//                             <Link to="/courses">All Courses</Link>
+//                         </li>
+//                         <li>
+//                             <Link to="/contact">Contact Us</Link>
+//                         </li>
+//                         <li>
+//                             <Link to="/about">About Us</Link>
+//                         </li>
+//                     </ul>
+//                 </div>
+//             </div>
+//             {children}
+//             <Footer />
+//         </div>
+//     );
+// }
+
+// export default HomeLayout;
+
+
+
+
+
+
+
--- client/src/Pages/HomePage.jsx
+++ client/src/Pages/HomePage.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import HomeLayout from '../Layouts/HomeLayout';
+import { Link } from 'react-router-dom';
+import HomePageImage from '../Assets/Images/homePageMainImage.png';
 
 function HomePage() {
     return (
@@ -13,6 +15,24 @@ function HomePage() {
                     <p className="text-xl text-gray-200">
                     We have a large library of courses taught by highly skilled and qualified faculties at a very affordable
                     </p>
+
+                    <div className="space-x-6">
+                        <Link to="/courses">
+                            <button className="bg-yellow-500 px-5 py-3 rounded-md font-semibold text-lg cursor-pointer hover:bg-yellow-600 transition-all ease-in-out duration-300">
+                                Explore courses
+                            </button>
+                        </Link>
+
+                        <Link to="/contact">
+                            <button className="border border-yellow-500 px-5 py-3 rounded-md font-semibold text-lg cursor-pointer hover:bg-yellow-600 transition-all ease-in-out duration-300">
+                                Contact Us
+                            </button>
+                        </Link>
+                    </div>
+                </div>
+
+                <div className="w-1/2 flex items-center justify-center">
+                    <img alt='homepage image' src={HomePageImage}  />
                 </div>
             </div>
         </HomeLayout>
