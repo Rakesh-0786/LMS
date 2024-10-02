a6b794c26fbf472145710e3e49ec1758c2469fe2
Rakesh-0786 <rakeshkum17253@gmail.com>
2024-10-03 04:00:12 +0530

    Added the 404 PageNotFound page and few changes in footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,4 +1,110 @@
 // import { BsFacebook, BsInstagram, BsLinkedin, BsTwitter } from 'react-icons/bs';
+
+// function Footer() {
+//     const currentDate = new Date();
+//     const year = currentDate.getFullYear();
+
+//     return (
+//         <>
+//             <footer className='fixed left-0 right-0 bottom-0 h-[10vh] py-5 flex flex-col sm:flex-row items-center justify-between text-white bg-gray-800 sm:px-20 w-full'>
+//                 <section className='text-lg'>
+//                     Copyright {year} | All rights reserved
+//                 </section>
+//                 <section className='flex items-center justify-center gap-5 text-2xl text-white'>
+//                     <a href='#' className='hover:text-yellow-500 transition-all ease-in-out duration-300'>
+//                         <BsFacebook />
+//                     </a>
+//                     <a href='#' className='hover:text-yellow-500 transition-all ease-in-out duration-300'>
+//                         <BsInstagram />
+//                     </a>
+//                     <a href='#' className='hover:text-yellow-500 transition-all ease-in-out duration-300'>
+//                         <BsLinkedin />
+//                     </a>
+//                     <a href='#' className='hover:text-yellow-500 transition-all ease-in-out duration-300'>
+//                         <BsTwitter />
+//                     </a>
+//                 </section>
+//             </footer>
+//         </>
+//     );
+// }
+
+// export default Footer;
+
+
+
+import { BsFacebook, BsInstagram, BsLinkedin, BsTwitter } from 'react-icons/bs';
+
+function Footer() {
+    const currentDate = new Date();
+    const year = currentDate.getFullYear();
+
+    return (
+        <>
+            <footer className='fixed left-0 right-0 bottom-0 h-[10vh] py-5 flex flex-col sm:flex-row items-center justify-between text-white bg-gray-800 sm:px-20 w-full'>
+                {/* Copyright Section */}
+                <section className='text-lg'>
+                    Copyright {year} | All rights reserved
+                </section>
+                
+                {/* Social Media Icons */}
+                <section className='flex items-center justify-center gap-5 text-2xl text-white'>
+                    {/* Facebook */}
+                    <a 
+                        href='https://www.facebook.com/profile.php?id=100041578942780' 
+                        target='_blank' 
+                        rel='noopener noreferrer' 
+                        className='hover:text-yellow-500 transition-all ease-in-out duration-300'>
+                        <BsFacebook />
+                    </a>
+                    
+                    {/* Instagram */}
+                    <a 
+                        href='https://www.instagram.com/your-instagram-id' 
+                        target='_blank' 
+                        rel='noopener noreferrer' 
+                        className='hover:text-yellow-500 transition-all ease-in-out duration-300'>
+                        <BsInstagram />
+                    </a>
+                    
+                    {/* LinkedIn */}
+                    <a 
+                        href='https://www.linkedin.com/in/rakesh-kumar-506b70246/' 
+                        target='_blank' 
+                        rel='noopener noreferrer' 
+                        className='hover:text-yellow-500 transition-all ease-in-out duration-300'>
+                        <BsLinkedin />
+                    </a>
+                    
+                    {/* Twitter */}
+                    <a 
+                        href='https://x.com/RakeshK99529429' 
+                        target='_blank' 
+                        rel='noopener noreferrer' 
+                        className='hover:text-yellow-500 transition-all ease-in-out duration-300'>
+                        <BsTwitter />
+                    </a>
+                </section>
+            </footer>
+        </>
+    );
+}
+
+export default Footer;
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { BsFacebook, BsInstagram, BsLinkedin, BsTwitter } from 'react-icons/bs';
 // function Footer() {
 //     const currentDate = new Date();
 //     const year = currentDate.getFullYear();
@@ -35,35 +141,3 @@
 
 
 
-import { BsFacebook, BsInstagram, BsLinkedin, BsTwitter } from 'react-icons/bs';
-
-function Footer() {
-    const currentDate = new Date();
-    const year = currentDate.getFullYear();
-
-    return (
-        <>
-            <footer className='fixed left-0 right-0 bottom-0 h-[10vh] py-5 flex flex-col sm:flex-row items-center justify-between text-white bg-gray-800 sm:px-20 w-full'>
-                <section className='text-lg'>
-                    Copyright {year} | All rights reserved
-                </section>
-                <section className='flex items-center justify-center gap-5 text-2xl text-white'>
-                    <a href='#' className='hover:text-yellow-500 transition-all ease-in-out duration-300'>
-                        <BsFacebook />
-                    </a>
-                    <a href='#' className='hover:text-yellow-500 transition-all ease-in-out duration-300'>
-                        <BsInstagram />
-                    </a>
-                    <a href='#' className='hover:text-yellow-500 transition-all ease-in-out duration-300'>
-                        <BsLinkedin />
-                    </a>
-                    <a href='#' className='hover:text-yellow-500 transition-all ease-in-out duration-300'>
-                        <BsTwitter />
-                    </a>
-                </section>
-            </footer>
-        </>
-    );
-}
-
-export default Footer;
