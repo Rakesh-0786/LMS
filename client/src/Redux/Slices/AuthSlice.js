36f408bc52086aa71f91e456a448679b32610006
Rakesh-0786 <rakeshkum17253@gmail.com>
2024-10-12 03:22:30 +0530

    Added Signin functionality

--- client/src/Redux/Slices/AuthSlice.js
+++ client/src/Redux/Slices/AuthSlice.js
@@ -4,8 +4,8 @@ import toast from "react-hot-toast";
 import axiosInstance from "../../Helpers/axiosInstance"
 const initialState ={
     isLoggedIn: localStorage.getItem('isLoggedIn') || false,
-    role:localStorage.getItem('role') || "",
-    data:localStorage.getItem('data') || {}
+    role: localStorage.getItem('role') || "",
+    data: localStorage.getItem('data') || {}
 };
 
 export const createAccount = createAsyncThunk("/auth/signup", async (data) => {
@@ -25,11 +25,38 @@ export const createAccount = createAsyncThunk("/auth/signup", async (data) => {
     }
 })
 
+export const login = createAsyncThunk("/auth/login", async (data) => {
+    try {
+        const res = axiosInstance.post("user/login", data);
+        toast.promise(res, {
+            loading: "Wait! authentication in progress...",
+            success: (data) => {
+                return data?.data?.message;
+            },
+            error: "Failed to log in"
+        });
+        return  (await res).data;
+    } catch(error) {
+        toast.error(error?.response?.data?.message);
+    }
+});
+
 const authSlice=createSlice({
     name:"auth",
     initialState,
     reducers: {},
+    extraReducers: (builder) => {
+        builder
+        .addCase(login.fulfilled, (state, action) => {
+            localStorage.setItem("data", JSON.stringify(action?.payload?.user));
+            localStorage.setItem("isLoggedIn", true);
+            localStorage.setItem("role", action?.payload?.user?.role);
+            state.isLoggedIn = true;
+            state.data = action?.payload?.user;
+            state.role = action?.payload?.user?.role
+        })
+    }
 });
 
-export const {}= authSlice.actions;
+// export const {}= authSlice.actions;
 export default authSlice.reducer;
