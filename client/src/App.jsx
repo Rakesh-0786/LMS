b7f5e6a255061610061029a329d12efbc36208f6
Rakesh-0786 <rakeshkum17253@gmail.com>
2024-10-16 16:36:40 +0530

    Added UserProfile Page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,6 +11,7 @@ import Denied from './Pages/Denied'
 import CourseDescription from './Pages/Course/CourseDescription'
 import RequireAuth from './components/Auth/RequireAuth'
 import CreateCourse from './Pages/Course/CreateCourse'
+import Profile from './Pages/User/Profile'
 
 function App() {
 
@@ -32,6 +33,10 @@ function App() {
 
       <Route element={<RequireAuth allowedRoles={["ADMIN"]}/>}>
       <Route path='/course/create' element={<CreateCourse />} />
+      </Route>
+
+      <Route element={<RequireAuth allowedRoles={["ADMIN", "USER"]}/>}>
+      <Route path='/user/profile' element={<Profile />} />
       </Route>
 
 
