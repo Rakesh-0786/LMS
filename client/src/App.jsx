cefe49c648351be754c8736761fe639831eccc11
Rakesh-0786 <rakeshkum17253@gmail.com>
2024-10-18 02:10:32 +0530

    Added Lecture creation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -17,6 +17,7 @@ import Checkout from './Pages/Payment/Checkout'
 import CheckoutSuccess from './Pages/Payment/CheckoutSuccess'
 import CheckoutFailure from './Pages/Payment/CheckoutFailure'
 import Displaylectures from './Pages/Dashboard/Displaylectures'
+import AddLecture from './Pages/Dashboard/Addlecture'
 
 function App() {
 
@@ -38,6 +39,8 @@ function App() {
 
       <Route element={<RequireAuth allowedRoles={["ADMIN"]}/>}>
       <Route path='/course/create' element={<CreateCourse />} />
+      <Route path='/course/addlecture' element={<AddLecture />} />
+
       </Route>
 
       <Route element={<RequireAuth allowedRoles={["ADMIN", "USER"]}/>}>
@@ -47,11 +50,6 @@ function App() {
       <Route path='/checkout/success' element={<CheckoutSuccess />} />
       <Route path='/checkout/fail' element={<CheckoutFailure />} />
       <Route path='/course/displaylectures' element={<Displaylectures />} />
-
-
-
-
-
       </Route>
 
 
--- client/src/Pages/User/Profile.jsx
+++ client/src/Pages/User/Profile.jsx
@@ -23,7 +23,7 @@ function Profile() {
     return (
         <HomeLayout>
             <div className="min-h-[90vh] flex items-center justify-center">
-                <div className="my-10 flex flex-col gap-4 rounded-lg p-4 text-white w-96 shadow-[0_0_10px_black]">
+                <div className="my-10 flex flex-col gap-4 rounded-lg p-4 text-white w-97 shadow-[0_0_10px_black]">
                     <img
                         src={userData?.avatar?.secure_url}
                         className="w-40 m-auto rounded-full border border-black"
